a1c6b9e924bfb4fbf97064d17e4965dd5ed1e021
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-16 00:31:54 +0200

    feat: fix notFOundMiddleware change home message

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,12 @@ import errorMiddleware from "./middleware/error-handler";
 const app = express();
 const port = +process.env.PORT || 3000;
 
-app.use(notFoundMiddleware);
 app.use(errorMiddleware);
 
 app.get("/", (_req, res) => {
-  res.send("Welcome to hell");
+  res.send("<h1>Store API</h1><a href='/api/v1/products'>Products Route</a>");
 });
+app.use("*", notFoundMiddleware);
 
 const start = async () => {
   try {
