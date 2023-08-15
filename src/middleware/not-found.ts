cbfadfd8a5aec702a45e6f0ecbf08ab7cfd151bc
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-16 00:01:47 +0200

    refactor: not-found to typescript

--- src/middleware/not-found.ts
+++ src/middleware/not-found.ts
@@ -1,3 +1,6 @@
-const notFound = (req, res) => res.status(404).send('Route does not exist')
+import { Request, Response } from "express";
 
-module.exports = notFound
+const notFound = (_req: Request, res: Response) =>
+  res.status(404).send("Route does not exist");
+
+module.exports = notFound;
