1e088d371835b77577d2911fa900e148282bf134
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-16 00:52:58 +0200

    refactor: move middlewares

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,11 @@ import errorMiddleware from "./middleware/error-handler";
 const app = express();
 const port = +process.env.PORT || 3000;
 
-app.use(errorMiddleware);
-
 app.get("/", (_req, res) => {
   res.send("<h1>Store API</h1><a href='/api/v1/products'>Products Route</a>");
 });
-app.use("*", notFoundMiddleware);
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
 const start = async () => {
   try {
