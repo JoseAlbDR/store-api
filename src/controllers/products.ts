89b757d08f4eac3dbe5390ea0d0c817b795963b0
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-16 16:40:43 +0200

    feat: getAllProductsStatic logic

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,8 +1,11 @@
 /* eslint-disable @typescript-eslint/require-await */
 import { Request, Response } from "express";
+import { Product } from "../models/product";
+import { IProduct } from "../types/interfaces";
 
 const getAllProductsStatic = async (_req: Request, res: Response) => {
-  res.status(200).json({ msg: "Products testing route" });
+  const products: IProduct[] = await Product.find();
+  res.status(200).json({ nbHits: products.length, products });
 };
 const getAllProducts = async (_req: Request, res: Response) => {
   res.status(200).json({ msg: "Products route" });
