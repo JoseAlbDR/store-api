b242025b5ccaac2077c72cd9819a62997dabb746
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-16 18:54:48 +0200

    refactor: move all Joi Request.productQuery validation to middleware

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,31 +1,15 @@
 /* eslint-disable @typescript-eslint/require-await */
 import { Request, Response } from "express";
 import { Product } from "../models/product";
-import { IProduct, IProductQuery } from "../types/interfaces";
-import { validateProductQuery } from "../utils/validation";
+import { IProduct } from "../types/interfaces";
 
 const getAllProductsStatic = async (_req: Request, res: Response) => {
   const products: IProduct[] = await Product.find();
   res.status(200).json({ nbHits: products.length, products });
 };
 
-const getAllProducts = async (
-  req: Request<unknown, unknown, unknown, IProductQuery>,
-  res: Response
-) => {
-  const { query } = req;
-  const valid = validateProductQuery(query);
-
-  if (valid.error) {
-    const messages = valid.error.details.map(
-      (detail): string => detail.message
-    );
-    return res
-      .status(400)
-      .json({ success: false, message: messages.join(", ") });
-  }
-
-  const products = await Product.find(valid.value);
+const getAllProducts = async (req: Request, res: Response) => {
+  const products = await Product.find(req.productQuery);
   return res.status(200).json({ nbHits: products.length, products });
 };
 
