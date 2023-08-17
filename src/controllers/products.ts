b8194e1092d160be64761e3077aae32040f5bda6
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-17 19:50:00 +0200

    feat: new object queryObject to save name and featured filters, sort logic implemented

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,25 +1,46 @@
 /* eslint-disable @typescript-eslint/require-await */
 import { Request, Response } from "express";
 import { Product } from "../models/product";
-import { IProduct } from "../types/interfaces";
+import { IProductQuery } from "../types/interfaces";
+// import { IProduct } from "../types/interfaces";
 
 const getAllProductsStatic = async (_req: Request, res: Response) => {
-  const search = "y";
-  const products: IProduct[] = await Product.find({
-    name: { $regex: search, $options: "i" },
-  });
+  const result = Product.find({}).sort("name");
+  console.log(result);
+  const products = await result;
   res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req: Request, res: Response) => {
+  const queryObject: IProductQuery = {};
+
   if (req.productQuery.name && typeof req.productQuery.name === "string") {
     req.productQuery.name = {
       $regex: req.productQuery.name,
       $options: "i",
     };
+    queryObject.name = req.productQuery.name;
   }
 
-  const products = await Product.find(req.productQuery);
+  if (
+    req.productQuery.featured &&
+    typeof req.productQuery.featured === "boolean"
+  ) {
+    queryObject.featured = req.productQuery.featured;
+  }
+
+  let result = Product.find(queryObject);
+
+  if (req.productQuery.sort && typeof req.productQuery.sort === "string") {
+    const { sort } = req.productQuery;
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  const products = await result;
+
   return res.status(200).json({ nbHits: products.length, products });
 };
 
